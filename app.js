4efb08bc22c56b02bf36b33c09e15a8e1155a462
Yannick Jost <yannick@yjost.com>
2026-02-17 13:27:59 +0100

    Node compatibility

--- app.js
+++ app.js
@@ -11,9 +11,30 @@ var http = require('http');
 var path = require('path');
 var pug = require('pug');
 var redirect = require("./routes/redirect");
+var fresh = require('fresh');
 
 
 var app = express();
+
+// Express 3 expects res._headers, which does not exist on modern Node.
+Object.defineProperty(express.request, 'fresh', {
+  configurable: true,
+  enumerable: true,
+  get: function() {
+    var method = this.method;
+    var status = this.res.statusCode;
+
+    if ('GET' !== method && 'HEAD' !== method) return false;
+    if ((status >= 200 && status < 300) || 304 === status) {
+      var responseHeaders = this.res._headers;
+      if (!responseHeaders && typeof this.res.getHeaders === 'function') {
+        responseHeaders = this.res.getHeaders();
+      }
+      return fresh(this.headers, responseHeaders || {});
+    }
+    return false;
+  }
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
