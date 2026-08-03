7a21878daef212e5e99302ecb19468614e216481
Yannick Jost <yannick@yjost.com>
2026-08-03 18:27:03 +0200

    Rename RobotKraft redirect

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.get('/cassini', redirect.redirect );
 app.get('/adhesion', redirect.redirect );
 app.get('/robotechgirls', redirect.redirect );
 app.get('/robokraft', redirect.redirect );
+app.get('/robotkraft', redirect.redirect );
 app.get('/semaineia', redirect.redirect );
 app.get('/work/:id', routes.showWork );
 app.get('/users', user.list);
